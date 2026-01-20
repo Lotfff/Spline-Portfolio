db537f0b41a75ac47d1c0f345ee253f5e7611ef1
Lotfff <ibraff739@gmail.com>
2026-01-20 17:33:51 +0100

    Update staticData.ts

--- client/src/lib/staticData.ts
+++ client/src/lib/staticData.ts
@@ -3,13 +3,13 @@ export const PROFILE_DATA = {
     id: "394912002843344898",
     username: "Lord",
     global_name: "Lord",
-    avatar: "/assets/pfp.jpg",
-    banner: "/assets/banner.gif",
+    avatar: "/pfp.jpg",
+    banner: "/banner.gif",
     accent_color: 5814783,
   },
   instagram: {
     username: "lordx679",
-    profile_pic: "/assets/pfp.jpg",
+    profile_pic: "/pfp.jpg",
     biography: "Turning impossible ideas into reality.",
     followers_count: 1337,
   }
